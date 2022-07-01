e105824f4b258dc5e68ca816831a97082e52631f
Basic38 <elenhovhannisyan24.08@gmail.com>
2022-07-01 22:05:52 +0400

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,63 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.Write("Hello");
+//Console.Write("Hello");
+//Console.WriteLine("2022");
+//Console.Write("Salt");
+
+
+//Console.Read();
+//Console.Write.Line("Input tt=");
+//Console.WriteLine("A+B=");
+
+//char v1 = "c";
+//char v2="v";
+//Console.WriteLine(v1);
+//Console.WriteLine(v2);
+
+//int myNum = 15;
+//    Console.WriteLine(myNum);
+
+
+
+
+//int a ;
+//int b = 3;
+//int c = a + b;
+//int d = 0;
+//int zero = b / d;
+//Console.WriteLine(zero);
+
+
+
+//int age = 18;
+//string firstName = "John";
+//string lastName = "Doe";
+//string fullName = firstName + lastName + age;
+//Console.WriteLine(fullName);
+
+
+
+//double dou=25.5554555555555555555555;
+//Console.WriteLine(dou);
+//float flo=26.555455555555555555555f;
+//Console.WriteLine(flo);
+//decimal dec=2753765.5554555555555555555555555555555555555m;
+//Console.WriteLine(dec);
+//Console.WriteLine(double.MaxValue);
+//Console.WriteLine(float.MaxValue);
+//Console.WriteLine(decimal.MaxValue);
+//decimal sum = (decimal)((double)flo + dou);
+
+
+//double dbtest = 28.6d;
+//string sttest = dbtest.ToString();
+//Console.WriteLine(sttest);
+
+
+double a= Convert.ToDouble(Console.ReadLine());
+double b= Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine(a + b);
+
+
+
